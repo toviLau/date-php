325490c03c4da66f6c6f3d33cba2b5f7082770a2
ToviLau <46134256@qq.com>
2020-05-21 11:05:37 +0800

    兼容 Typescript

--- @types/date.d.ts
+++ @types/date.d.ts
@@ -1,13 +1,13 @@
-export = date_php;
+export default date_php;
 
-declare function date_php(fmt?: string, now?: any, ms?: boolean): string;
+declare function date_php(tplChars: string, timeObj: string | number | object, ms?: boolean): string;
 
 declare namespace date_php {
     const version: string;
     function description(): any;
 
-    function countTime(fmt?: string, timestamp1?: number, timestamp2?: number, ms?: boolean): string;
+    function countTime(tplChars?: string, timestamp1?: number, timestamp2?: number, ms?: boolean): string;
 
-    function duration(fmt?: string, timestamp?: number, ms?: boolean): string;
+    function duration(tplChars?: string, timestamp?: number, ms?: boolean): string;
 }
 
